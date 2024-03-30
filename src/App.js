5a8cb792a87663613a62162ed40c54e3d864ab95
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-03-30 16:25:21 +0000

    Code searching machinery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} alt="logo" className="App-logo img-fluid" />
-      Hello
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Irene Caldera Quevedo</small>
+        </footer>
+      </div>
     </div>
   );
 }
