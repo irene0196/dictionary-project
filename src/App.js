9925941704cb166231b9a6260b50c516cf092eb1
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-03-30 13:15:34 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} alt="logo" className="App-logo img-fluid" />
+      Hello
+    </div>
+  );
+}
