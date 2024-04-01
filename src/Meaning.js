93a88aa1bc608b0b97b49e371b2acae870635dda
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-04-01 15:00:31 +0100

    Styled the application

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,14 +9,9 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition: </strong>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
