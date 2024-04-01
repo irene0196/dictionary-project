78b046ca9a970639827728a46a548ebd0bcc974e
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-04-01 13:42:28 +0100

    Phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,6 @@ import "./Meaning.css";
 import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
-  console.log(props);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
